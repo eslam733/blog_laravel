685f752677ba2c6af179966b96a639a19848b71e
Eslam Mohamed <emohamef733@gmail.com>
2023-03-05 00:54:31 +0200

    get posts from db

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Post;
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('blog.index');
+        return view('blog.index')->with('posts', Post::orderBy('created_at', 'ASC')->get());
     }
 
     public function create()
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,49 +4,29 @@
     <div class="container m-auto text-center pt-15 pb-5">
         <h1 class="text-6xl font-bold">All Posts</h1>
     </div>
-
-    <div class="container sm:grid grid-cols-2 gap-15 mx-auto py-15 px-5 border-b border-gray-400">
-        <div class="flex">
-            <img class="object-cover" src="https://picsum.photos/seed/picsum/920/620" alt="">
+    @auth
+        <div class="mx-auto text-center py-12"><a class="bg-blue-700 text-gray-100 py-4 px-5 rounded-lg font-bold uppercase place-self-start"
+                href="/blog/create">Create Post</a></div>
+    @endauth
+    @foreach ($posts as $post)
+        <div class="container sm:grid grid-cols-2 gap-15 mx-auto py-15 px-5 border-b border-gray-400">
+            <div class="flex">
+                <img class="object-cover" src="{{ $post->image }}" alt="">
+            </div>
+            <div class="pb-10">
+                <h2 class="text-gray-700 font-bold text-4xl py-5 md:pt-0">{{ $post->title }}</h2>
+                <span>
+                    By: <span class="text-gray-500 italic">{{ $post->user->name }}</span><br>
+                    <span class="mt-2 block">On: <span
+                            class="text-gray-500 italic">{{ date('Y-m-d', strtotime($post->updated_at)) }}</span></span>
+                    <p class="text-l text-gray-700 py-8 leading-6">
+                        {{ $post->description }}
+                    </p>
+                </span>
+                <a class="bg-gray-700 text-gray-100 py-4 px-5 rounded-lg font-bold uppercase place-self-start"
+                    href="/blog/{{ $post->slug }}">Read
+                    More</a>
+            </div>
         </div>
-        <div class="pb-10">
-            <h2 class="text-gray-700 font-bold text-4xl py-5 md:pt-0">How create laravel Project</h2>
-            <span>
-                By: <span class="text-gray-500 italic">Eslam Mohamed</span>
-                <p class="text-l text-gray-700 py-8 leading-6">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                    scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-                    into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                    release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                    software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-            </span>
-            <a class="bg-gray-700 text-gray-100 py-4 px-5 rounded-lg font-bold uppercase place-self-start"
-                href="">Read
-                More</a>
-        </div>
-    </div>
-    <div class="container sm:grid grid-cols-2 gap-15 mx-auto py-15 px-5">
-        <div class="flex">
-            <img class="object-cover" src="https://picsum.photos/seed/picsum/920/620" alt="">
-        </div>
-        <div class="pb-10">
-            <h2 class="text-gray-700 font-bold text-4xl py-5 md:pt-0">How create laravel Project</h2>
-            <span>
-                By: <span class="text-gray-500 italic">Eslam Mohamed</span>
-                <p class="text-l text-gray-700 py-8 leading-6">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                    scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-                    into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                    release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
-                    software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
-            </span>
-            <a class="bg-gray-700 text-gray-100 py-4 px-5 rounded-lg font-bold uppercase place-self-start"
-                href="">Read
-                More</a>
-        </div>
-    </div>
+    @endforeach
 @endsection
